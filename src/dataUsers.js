d5812fe7b26fac6b28c9b0f444d86e8990f10e34
saradvr <saradvr1608@gmail.com>
2021-03-03 16:44:17 -0500

    creacion coachcard y coacheslist componentes

---
+++ src/dataUsers.js
@@ -0,0 +1,44 @@
+export const dataUsers = [
+  { 
+    _id: '9f94d491-64b2-48ff-8766-426abdca11d1',
+    email: 'samir@mail.com',
+    password: '12345',
+    _idUserType: 'e2f2a94f-7c2a-40d4-b9f0-2e06d8d6dc74',
+    },
+  { 
+    _id: 'd155a0e3-718c-4d25-b1d6-2f192195fe24',
+    email: 'sara@mail.com',
+    password: '54321',
+    _idUserType: '342551a5-8c37-4df2-b72f-52e243be04f9',
+  },
+  { 
+    _id: '535355cf-b73f-4412-8a84-11d7a8a2bc6e',
+    email: 'camila@mail.com',
+    password: '21734819',
+    _idUserType: 'bf2227aa-ae53-4555-af13-6ebd09295bdd',
+  },
+  { 
+    _id: 'a7906bfe-1a33-4177-a57b-4407dec218b9',
+    email: 'nat@mail.com',
+    password: 'wiugbwieub',
+    _idUserType: '5899abd1-0700-4e21-95c2-f0581ef2c694',
+  },
+  { 
+    _id: 'db922ff4-d175-4962-b5fd-b4f3037e4d50',
+    email: 'sant@mail.com',
+    password: 'qiuefbiew',
+    _idUserType: '7c69b1b2-8550-4ba8-80b3-300cab17c9d7',
+  },
+  { 
+    _id: '89151342-d3c5-4ebb-90ba-80329e6c82d3',
+    email: 'm@mail.com',
+    password: 'wiubiwubqv',
+    _idUserType: '96c8f366-851b-4774-af24-e4dbaf03bf4b',
+  },
+  { 
+    _id: '4d0c3ff5-1a08-427a-b434-84b34f112bde', 
+    email: 'luis@mail.com',
+    password: 'fqwryeqrybe',
+    _idUserType: '82319926-72f0-40c5-88f0-7de4288e71b5',
+  },
+]
