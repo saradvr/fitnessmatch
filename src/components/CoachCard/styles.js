d17d17fa8e09466f279c2c68626d853d0c27ce6b
saradvr <75816184+saradvr@users.noreply.github.com>
2021-03-10 10:30:26 -0500

    estilos Coach card (#10)

--- src/components/CoachCard/styles.js
+++ src/components/CoachCard/styles.js
@@ -1,12 +1,20 @@
 import styled from 'styled-components';
 
 export const StyledArticle = styled.article`
-  border: 4px solid #69B32D;
   border-radius: 20px;
   margin: 20px;
   padding: 20px;
+  background-color: #FBFCF9;
+  width: auto;
+  font-family: "Roboto";
+  color: #0B1517;
 `
 
 export const StyledH2 = styled.h2`
-  margin: 0;
+  margin: 10px 0 10px 0;
+`
+
+export const StyledTd = styled.td`
+  text-align: ${props => props.alignCenter ? 'center' : props.alignRight ? 'right' : 'left'};
+  padding: 0 10px 0 10px;
 `
---
+++ src/components/LinkButton/index.js
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+export const LinkButton = styled(Link)`
+  border: 1px solid ${props => props.green ? "white" : "#69B32D"};
+  background-color: ${props => props.green ? "#69B32D" : "none"};
+  color: #FBFCF9;
+  text-decoration: none;
+  padding: 10px;
+  width: 145px;
+  display: inline-block;
+  max-width: 100%;
+  font-size: 18px;
+  font-family: "Roboto";
+  font-weight: 700;
+  border-radius: 5.5px;
+  text-align: center;
+
+  @media all and (max-width:420px){
+    display: block;
+    margin: 0 0 0 auto;
+    width: auto;
+  }
+`
