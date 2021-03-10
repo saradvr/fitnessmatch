b91754523e5fe84031346c06717ae37a03ccdad1
Carlos Valencia <rasfreed@gmail.com>
2021-03-10 18:40:27 -0500

    Se aplicó un collapse a los 3 filtros

--- src/components/FilterContainer.js
+++ src/components/FilterContainer.js
@@ -1,3 +1,5 @@
+import React, {useState, useCallback} from 'react';
+import {Collapse} from 'react-collapse'
 import Filter from "./Filter";
 import Button from "./Button";
 
@@ -11,29 +13,54 @@ function FilterContainer({
   handleChange,
   handleSubmit,
 }){
+
+  const [isCheckboxCollapseOpen1, setIsCheckboxCollapseOpen1] = useState(false);
+  const [isCheckboxCollapseOpen2, setIsCheckboxCollapseOpen2] = useState(false);
+  const [isInputsCollapseOpen, setIsInputsCollapseOpen] = useState(false);
+
+  const onClick1 = useCallback(
+    () => setIsCheckboxCollapseOpen1(!isCheckboxCollapseOpen1),
+    [isCheckboxCollapseOpen1]
+  )
+
+  const onClick2 = useCallback(
+    () => setIsCheckboxCollapseOpen2(!isCheckboxCollapseOpen2),
+    [isCheckboxCollapseOpen2]
+  )
+
+  const onClick3 = useCallback(
+    () => setIsInputsCollapseOpen(!isInputsCollapseOpen),
+    [isInputsCollapseOpen]
+  )
+
   return(
     <form onSubmit={handleSubmit}>
-      <h2>Objetivos</h2>
-      <Filter
-        filterName={specializations}
-        nameCheckbox='checkSpecializations'
-        checks = {checkSpecializations}
-        handleChange = {handleChange}
-      />
-      <h2>Disciplinas</h2>
-      <Filter
-        filterName={disciplines}
-        nameCheckbox='checkDisciplines'
-        checks = {checkDisciplines}
-        handleChange = {handleChange}
-      />
-      <h2>Rangos de precios</h2>
+      <h2 onClick={onClick1}>Objetivos</h2>
+      <Collapse isOpened={isCheckboxCollapseOpen1}>
+        <Filter
+          filterName={specializations}
+          nameCheckbox='checkSpecializations'
+          checks = {checkSpecializations}
+          handleChange = {handleChange}
+        />
+      </Collapse>
+      <h2 onClick={onClick2}>Disciplinas</h2>
+      <Collapse isOpened={isCheckboxCollapseOpen2}>
+        <Filter
+          filterName={disciplines}
+          nameCheckbox='checkDisciplines'
+          checks = {checkDisciplines}
+          handleChange = {handleChange}
+        />
+      </Collapse>
+      <h2 onClick={onClick3}>Rangos de precios</h2>
+      <Collapse isOpened={isInputsCollapseOpen}>
       <label htmlFor="minimo">Mínimo</label>
       <input type="text" id="minimo" name="minFee" value={minFee} onChange={handleChange} /> 
         - 
       <input type="text" id="maximo" name="maxFee" value={maxFee} onChange={handleChange} /> 
       <label htmlFor="maximo">Máximo</label>
-
+      </Collapse>
       <Button
         type="submit"
       >
