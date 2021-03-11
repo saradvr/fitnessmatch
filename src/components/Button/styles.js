51e7933225deb72d0ae1897052a82ee52648de46
saradvr <75816184+saradvr@users.noreply.github.com>
2021-03-11 08:15:24 -0500

    agrega estilos al botón (#12)
    * agrega estilos al botón
    
    * fondo transparente para el botón

---
+++ src/components/Button/styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const StyledButton = styled.button`
+  border: 1px solid #FBFCF9;
+  background-color: ${props => props.green ? "#69B32D" : "transparent"};
+  color: #FBFCF9;
+  text-decoration: none;
+  padding: 10px;
+  width: 145px;
+  display: inline-block;
+  max-width: 100%;
+  font-size: 18px;
+  font-family: "Roboto";
+  font-weight: 700;
+  border-radius: 5.5px;
+  text-align: center;
+`
