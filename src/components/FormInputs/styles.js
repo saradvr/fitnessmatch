95dc5277a5367d45359c2d422afea167204b8b7b
saradvr <75816184+saradvr@users.noreply.github.com>
2021-03-12 10:34:49 -0500

    Estilos página Login (#18)
    * correccion importacion de estilo pagina login
    
    * Modificación de estilos login

--- src/components/FormInputs/styles.js
+++ src/components/FormInputs/styles.js
@@ -4,10 +4,10 @@ export const StyledInput = styled.input`
   border-radius: 5.5px;
   background-color: #FBFCF9;
   color: #0B1517;
-  font-size: 18px;
+  font-size: 14px;
   font-family: "Roboto";
   font-weight: 300;
-  padding: 14px 5px 14px 5px;
+  padding: 10px;
   display: block;
   width: 100%;
   border: none;
@@ -16,7 +16,7 @@ export const StyledInput = styled.input`
 export const StyledLabel = styled.label`
   font-family: Roboto;
   font-weight: 700;
-  font-size: 22px;
+  font-size: 16px;
   color: #FBFCF9;
   display: block;
   margin: 10px auto 10px auto;
