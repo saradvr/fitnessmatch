3f57e111e9b7526e304abf5e92ffc0e0226c8136
Carlos Valencia <rasfreed@gmail.com>
2021-03-11 20:51:16 -0500

    Se agregaron estilos al componente filter

---
+++ src/components/FilterContainer/styles.js
@@ -0,0 +1,56 @@
+import styled from 'styled-components';
+
+export const StyledFiltrosH2 = styled.h2`
+  display: block;
+  font-size: 22px;
+  color: #0B1517;
+  font-family: 'Roboto';
+  text-align: center;
+`
+
+export const StyledTitle = styled.h2`
+  display: block;
+  font-size: 22px;
+  color: #0B1517;
+  font-family: 'Roboto';
+
+  &:hover {
+    cursor: pointer;
+  }
+`
+
+export const StyledCaret = styled.i`
+  border: solid #69B32D;
+  border-width: 0 3px 3px 0;
+  display: inline-block;
+  padding: 3px;
+  transform: rotate(45deg);
+  -webkit-transform: rotate(45deg);
+  margin-left: 4px;
+  margin-bottom: 5px;
+`
+
+export const StyledSection = styled.section`
+  background-color: #FBFCF9;
+  padding: 15px;
+  border-radius: 20px;
+`
+
+export const StyledLabels = styled.label`
+font-size: 18px;
+color: #0B1517;
+font-family: 'Roboto';
+margin: 10px;
+`
+
+export const StyledInput = styled.input`
+  border-radius: 5.5px;
+  background-color: #FBFCF9;
+  color: #0B1517;
+  font-size: 18px;
+  font-family: "Roboto";
+  font-weight: 300;
+  padding: 10px;
+  border: 1px solid #69B32D;
+  margin-bottom: 15px;
+  `
